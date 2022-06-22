a5364eb3b075d33bbe460453a1fb85a53fb85537
werlang <pswerlang@gmail.com>
2022-06-22 02:32:19 +0000

    Update file for ease of use.

--- 10-acessando-bd-pdo/02-consultas/index.php
+++ 10-acessando-bd-pdo/02-consultas/index.php
@@ -3,17 +3,18 @@
 require __DIR__ . "/source/Boot/config.php";
 require __DIR__ . "/source/autoload.php";
 
-use Source\Database\Connect;
+// use Source\Database\Connect;
+$db = \Source\Database\Connect::getInstance();
 
 echo "<h2>INSERT com Query</h2>";
 
 $insert = "INSERT INTO users (name, email, password) VALUES ('Fábio Luís da Silva Santos','fabiosantos@ifsul.edu.br','23465')";
 
 try{
-    $query = Connect::getInstance()->query($insert);
+    $query = $db->query($insert);
     var_dump(
         $query,
-        Connect::getInstance()->lastInsertId(),
+        $db->lastInsertId(),
         $query->errorInfo()
     );
 }catch (PDOException $exception) {
@@ -26,7 +27,7 @@ echo "<h2>SELECT com Query</h2>";
 $select = "SELECT * FROM users LIMIT 4";
 
 try {
-    $query = Connect::getInstance()->query($select);
+    $query = $db->query($select);
     var_dump([
         $query,
         $query->rowCount(),
@@ -41,7 +42,7 @@ echo "<h2>UPDATE com Exec</h2>";
 $update = "UPDATE users SET name = 'Carlos Eduardo Cunnha' WHERE id = 5";
 
 try {
-   $query = Connect::getInstance()->exec($update);
+   $query = $db->exec($update);
    var_dump($query);
 } catch (PDOException $exception) {
     var_dump($exception);
@@ -52,7 +53,7 @@ echo "<h2>DELETE com Exec</h2>";
 $delete = "DELETE FROM users WHERE id > 5";
 
 try {
-    $query = Connect::getInstance()->exec($delete);
+    $query = $db->exec($delete);
     var_dump($query);
 } catch (PDOException $exception) {
     var_dump($exception);
