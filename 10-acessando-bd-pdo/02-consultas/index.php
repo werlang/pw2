6bc8a48156d3f231dfe65868d66ea1acf448c1e4
Fábio Santos <fabio3268@gmail.com>
2022-06-23 10:11:04 -0300

    Acessando bd com PDO

--- 10-acessando-bd-pdo/02-consultas/index.php
+++ 10-acessando-bd-pdo/02-consultas/index.php
@@ -9,52 +9,37 @@ echo "<h2>INSERT com Query</h2>";
 
 $insert = "INSERT INTO users (name, email, password) VALUES ('Fábio Luís da Silva Santos','fabiosantos@ifsul.edu.br','23465')";
 
-try{
-    $query = Connect::getInstance()->query($insert);
-    var_dump(
-        $query,
-        Connect::getInstance()->lastInsertId(),
-        $query->errorInfo()
-    );
-}catch (PDOException $exception) {
-    var_dump($exception);
-}
+$query = Connect::getInstance()->query($insert);
+var_dump(
+    $query,
+    Connect::getInstance()->lastInsertId(),
+    $query->errorInfo()
+);
 
 
 echo "<h2>SELECT com Query</h2>";
 
 $select = "SELECT * FROM users LIMIT 4";
 
-try {
-    $query = Connect::getInstance()->query($select);
-    var_dump([
-        $query,
-        $query->rowCount(),
-        $query->fetchAll()
-    ]);
-} catch (PDOException $exception){
-    var_dump($exception);
-}
+
+$query = Connect::getInstance()->query($select);
+var_dump([
+    $query,
+    $query->rowCount(),
+    $query->fetchAll()
+]);
 
 echo "<h2>UPDATE com Exec</h2>";
 
 $update = "UPDATE users SET name = 'Carlos Eduardo Cunnha' WHERE id = 5";
 
-try {
-   $query = Connect::getInstance()->exec($update);
-   var_dump($query);
-} catch (PDOException $exception) {
-    var_dump($exception);
-}
+$query = Connect::getInstance()->exec($update);
+var_dump($query);
+
 
 echo "<h2>DELETE com Exec</h2>";
 
-$delete = "DELETE FROM users WHERE id > 5";
+//$delete = "DELETE FROM users WHERE id > 5";
 
-try {
-    $query = Connect::getInstance()->exec($delete);
-    var_dump($query);
-} catch (PDOException $exception) {
-    var_dump($exception);
-}
-
+//$query = Connect::getInstance()->exec($delete);
+//var_dump($query);
