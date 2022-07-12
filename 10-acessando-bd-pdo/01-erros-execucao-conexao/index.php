19a3889206e91e6c1e5cff7fde01c58cc942c719
werlang <pswerlang@gmail.com>
2022-07-12 22:49:18 +0000

    Change exceptions lesson.

--- 10-acessando-bd-pdo/01-erros-execucao-conexao/index.php
+++ 10-acessando-bd-pdo/01-erros-execucao-conexao/index.php
@@ -1,33 +1,34 @@
 <?php
 
-try {
+// try {
 
-} catch (PDOException $exception){
-    var_dump($exception);
-}
-finally {
-    echo "<p>Execução Terminou!</p>";
-}
+// } catch (PDOException $exception){
+//     var_dump($exception);
+// }
+// finally {
+//     echo "<p>Execução Terminou!</p>";
+// }
 
-echo "---------------";
+// echo "---------------";
 
 try {
     $pdo = new PDO(
         "mysql:host=localhost;dbname=bd-pwii",
         "root",
-        "",
-        [
-            PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
-        ]
+        ""
     );
 
     $stmt = $pdo->query("SELECT * FROM users LIMIT 3");
 
     while ($user = $stmt->fetch(PDO::FETCH_ASSOC)){ // mode do fetch PDO::FETCH_ASSOC
+        echo "<pre>";
         var_dump($user);
+        echo "</pre>";
     }
-
+    
 } catch (PDOException $exception) {
+    echo "<pre>";
     var_dump($exception);
+    echo "</pre>";
     echo "<p>{$exception->getMessage()}</p>";
 }
